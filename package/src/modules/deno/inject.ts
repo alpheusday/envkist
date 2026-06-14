0df81f732e176fb732a143de132200a54ac95505
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-06-14 20:00:22 +0800

    refactor: remove unused deno const

--- package/src/modules/deno/inject.ts
+++ package/src/modules/deno/inject.ts
@@ -1,11 +1,5 @@
 import { createInject } from "#/functions/inject";
 
-declare const Deno: {
-    env: {
-        get(name: string): string | undefined;
-    };
-};
-
 const denoInjectAccessor = (name: string): string => {
     return `Deno.env.get("${name}")`;
 };
